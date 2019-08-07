e0127c7a32b6f1794ed590bf180849871bb48a0f
justHungryMan <justHungryMan@gmail.com>
2019-08-08 00:11:09 +0900

    [DONE] SVM classifier

--- assignment1/cs231n/classifiers/linear_classifier.py
+++ assignment1/cs231n/classifiers/linear_classifier.py
@@ -57,8 +57,10 @@ class LinearClassifier(object):
             #########################################################################
             # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-            pass
-
+            batch_index = np.random.choice(np.arange(num_train), batch_size, replace=False)
+            X_batch = X[batch_index]
+            y_batch = y[batch_index]
+            
             # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
             # evaluate loss and gradient
@@ -72,7 +74,7 @@ class LinearClassifier(object):
             #########################################################################
             # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-            pass
+            self.W -= learning_rate * grad
 
             # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -102,7 +104,8 @@ class LinearClassifier(object):
         ###########################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-        pass
+        scores = X.dot(self.W)
+        y_pred = np.argmax(scores, axis = 1)
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         return y_pred
--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -36,14 +36,21 @@ def svm_loss_naive(W, X, y, reg):
             margin = scores[j] - correct_class_score + 1 # note delta = 1
             if margin > 0:
                 loss += margin
+                dW[:, j] += X[i, :]
+                dW[:, y[i]] -= X[i, :]
+    # 이거 개어려움, analytic gradient 를 적용해야함.
+    # https://mlxai.github.io/2017/01/06/vectorized-implementation-of-svm-loss-and-gradient-update.html 여기 참고..
+                   
 
     # Right now the loss is a sum over all training examples, but we want it
     # to be an average instead so we divide by num_train.
     loss /= num_train
+    dW /= num_train
 
     # Add regularization to the loss.
     loss += reg * np.sum(W * W)
-
+    dW += reg * 2 * W
+    
     #############################################################################
     # TODO:                                                                     #
     # Compute the gradient of the loss function and store it dW.                #
@@ -77,8 +84,21 @@ def svm_loss_vectorized(W, X, y, reg):
     # result in loss.                                                           #
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-
-    pass
+    
+    scores = np.dot(X, W)
+    # N * 1
+    # x = np.reshape(x, (len(x),-1))
+    
+    correct_class_scores = scores[np.arange(X.shape[0]), y].T
+    # (500, ) to (500, 1)
+    correct_class_scores = np.reshape(correct_class_scores, (len(correct_class_scores), -1))
+    # SVM
+    margins = np.maximum(0, scores - correct_class_scores + 1)
+    # j = y_i 성분 지우기
+    margins[np.arange(X.shape[0]), y] = 0
+    # loss
+    loss = np.mean(margins)
+    
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -92,8 +112,19 @@ def svm_loss_vectorized(W, X, y, reg):
     # loss.                                                                     #
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+    binary = margins
+    #  xw - xw_{y_i} + 1 > 0 인 구간을 1로 표시.
+    binary[margins > 0] = 1
+    # 위의 margin 구할 때 - X[i, :] 한 만큼을 구함.
+    row_sum = np.sum(binary, axis=1)
+    binary[np.arange(X.shape[0]), y] = -row_sum.T
+    dW = np.dot(X.T, binary)
 
-    pass
+    # Average
+    dW /= X.shape[0]
+
+    # Regularize
+    dW += reg * 2 * W
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
